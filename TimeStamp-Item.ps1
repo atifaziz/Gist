b0649bd90805f0985cc220bd40073a01d4bb408d
Atif Aziz <code@raboof.com>
2013-06-13 23:32:12 -0700

    PowerShell script to add a UTC timestamp at the end of a file's name

---
+++ TimeStamp-Item.ps1
@@ -0,0 +1,36 @@
+function TimeStamp-Item
+{
+    [CmdletBinding(SupportsShouldProcess = $true, ConfirmImpact = 'Low')]
+    param(
+        [Parameter(Mandatory = $true, ValueFromPipeline = $true,
+                   HelpMessage = "Specifies the path to the item to timestamp")]
+        [string[]]$Path,
+        [switch]$Force = $false
+    )
+
+    PROCESS 
+    {
+        ForEach ($path In $path) 
+        {
+            $file = Get-Item $path -ea Stop | ? { (-not $_.PSIsContainer) }
+            if ($file -eq $null)
+            {
+                Write-Verbose "Skipping directory: $path"
+                continue
+            }
+            $ext = $file.Extension
+            $baseName = [IO.Path]::GetFileNameWithoutExtension($file.Name)
+            if ($baseName -match '^(.+?)_[0-9]{8}_[0-9]{6}$')
+            {
+                if (-not $force) 
+                { 
+                    Write-Verbose "Skipping file that already appears time-stamped: $path"
+                    continue 
+                }
+                $baseName = $matches[1]                
+            }            
+            $newName = "$($baseName)_$($file.LastWriteTimeUtc.ToString('yyyyMMdd_HHmmss'))$ext"            
+            Rename-Item $file $newName
+        }
+    }
+}
