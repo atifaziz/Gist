b8efebbb6929761dcc5cdba4b88c74d7dfb5fc5a
Atif Aziz <code@raboof.com>
2014-03-06 06:02:00 -0800

    Initial import of Get-ProcessTree for PowerShell

---
+++ Get-ProcessTree.ps1
@@ -0,0 +1,53 @@
+# Copyright (c) 2014 Atif Aziz. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Adapted from http://p0w3rsh3ll.wordpress.com/2012/10/12/show-processtree/
+
+function Get-ProcessTree
+{
+    [CmdletBinding()]
+    param([string]$ComputerName,
+          [int]$IndentSize = 2)
+    
+    $computerName = ($computerName, ".")[[String]::IsNullOrEmpty($computerName)]
+    $processes = Get-WmiObject Win32_Process -ComputerName $computerName
+    $pids = $processes | select -ExpandProperty ProcessId
+    $parents = $processes | select -ExpandProperty ParentProcessId -Unique
+    $liveParents = $parents | ? { $pids -contains $_ }
+    $deadParents = Compare-Object -ReferenceObject $parents -DifferenceObject $liveParents | select -ExpandProperty InputObject
+
+    function Write-ProcessTree($process, [int]$level = 0)
+    {
+        $id = $process.ProcessId
+        $parentProcessId = $process.ParentProcessId
+        $process = Get-Process -Id $id -ComputerName $computerName
+        $indent = New-Object String(' ', ($level * $indentSize))
+        $label = "$indent$($process.Name)"
+        $process `
+        | Add-Member NoteProperty ParentId $parentProcessId -PassThru `
+        | Add-Member NoteProperty Level $level -PassThru `
+        | Add-Member NoteProperty IdentedName $label -PassThru 
+        $processes `
+        | ? { $_.ParentProcessId -eq $id } `
+        | % { Write-ProcessTree $_ ($level + 1) }
+    }
+
+    $processes `
+    | ? { $_.ProcessId -ne 0 -and ($_.ProcessId -eq $_.ParentProcessId -or $deadParents -contains $_.ParentProcessId) } `
+    | % { Write-ProcessTree $_ }
+}
+
+<# Usage: 
+Get-ProcessTree | select Id, Level, IdentedName, ParentId
+#>
