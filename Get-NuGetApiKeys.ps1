f4a65de87b3d8829cb7ab227870074191db8f940
Atif Aziz <code@raboof.com>
2015-12-11 10:06:50 +0100

    Initial import of PowerShell script to list NuGet API keys

---
+++ Get-NuGetApiKeys.ps1
@@ -0,0 +1,10 @@
+Add-Type -AssemblyName System.Security
+$config = [xml](Get-Content (Join-Path (Join-Path $env:AppData NuGet) NuGet.config))
+$config.configuration.apikeys.add | % { 
+    $entropy = [Text.Encoding]::UTF8.GetBytes('NuGet')
+    $value = [Security.Cryptography.ProtectedData]::Unprotect([Convert]::FromBase64String($_.value), $entropy, [System.Security.Cryptography.DataProtectionScope]::CurrentUser)
+    New-Object PSObject -Property @{
+        Source = $_.key
+        ApiKey = [Text.Encoding]::UTF8.GetString($value)
+    }
+}
