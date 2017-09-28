32db66869a5989623799ff254c4fb0ebdf1a0cbd
Atif Aziz <code@raboof.com>
2017-09-28 10:43:33 +0200

    Script to list top git objects

---
+++ gittop.ps1
@@ -0,0 +1,12 @@
+﻿[CmdletBinding()]
+param([int]$Top = 10, [switch]$LongHash = $false)
+
+git rev-list --objects --all |
+    git cat-file '--batch-check=%(objecttype) %(objectname) %(objectsize) %(rest)' |
+    % { $tokens = ($_ -split ' ', 4);
+        New-Object psobject -Property @{ Kind = $tokens[0];
+                                         Hash = $(if ($longHash) { $tokens[1] } else { $tokens[1].Substring(0, 7) });
+                                         Size = [int]$tokens[2];
+                                         Name = $tokens[3] } } |
+    sort -Descending Size |
+    select -First $Top Kind, Size, Hash, Name
